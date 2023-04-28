8959d168452a6403ef5e29281ad99d5781752982
chosunghyun18 <krismon@naver.com>
2023-04-28 10:13:38 +0900

    fix: fix aws graddle version

---
+++ travelfeeldog/src/main/java/travelfeeldog/infra/aws/s3/S3Component.java
@@ -0,0 +1,14 @@
+package travelfeeldog.infra.aws.s3;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+@Component
+public class S3Component {
+    private String bucket;
+}
