0d2f7e59e8183b338baed6ce8d5e0987b4f4675b
chosunghyun18 <krismon@naver.com>
2023-04-28 11:01:08 +0900

    feat :add member id and nickname

--- travelfeeldog/src/main/java/travelfeeldog/domain/member/model/Member.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/member/model/Member.java
@@ -1,6 +1,24 @@
 package travelfeeldog.domain.member.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import travelfeeldog.global.common.model.BaseTimeEntity;
-
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", length = 64) //pk
+    private Long id;
+
+    @Column(name = "nick_name", length = 100)
+    private String nickName;
 }
