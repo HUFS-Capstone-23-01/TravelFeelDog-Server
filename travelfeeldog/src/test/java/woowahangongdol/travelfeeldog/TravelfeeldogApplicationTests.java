33cbb24e351444c625f733880f366571198c9054
chosunghyun18 <krismon@naver.com>
2023-04-28 09:31:54 +0900

    feat : inital settings -infra : aws -domain : feed , member,place,review atc

---
+++ travelfeeldog/src/test/java/woowahangongdol/travelfeeldog/TravelfeeldogApplicationTests.java
@@ -0,0 +1,13 @@
+package woowahangongdol.travelfeeldog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelfeeldogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
