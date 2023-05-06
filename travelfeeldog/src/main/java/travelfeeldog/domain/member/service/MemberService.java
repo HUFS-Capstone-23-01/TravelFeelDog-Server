9203659f2d334734cfa4770734c5d0987bfd8fe9
seungyeobchoi <98461038+seungyeobchoi@users.noreply.github.com>
2023-05-06 23:57:03 +0900

    feat : add method for MemberService

--- travelfeeldog/src/main/java/travelfeeldog/domain/member/service/MemberService.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/member/service/MemberService.java
@@ -1,4 +1,64 @@
 package travelfeeldog.domain.member.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import travelfeeldog.domain.member.dao.MemberRepository;
+import travelfeeldog.domain.member.model.Member;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member saveMember(String nickName, String imageUrl, String token) {
+        return memberRepository.saveMember(nickName, 1, 0, imageUrl, token)
+                .orElseThrow(() -> new RuntimeException("Member not saved"));
+    }
+
+    @Transactional
+    public void deleteMember(String token) {
+        memberRepository.deleteMember(token);
+    }
+
+    @Transactional
+    public Member updateImageUrl(String token, String imageUrl) {
+        return memberRepository.updateMemberImageUrl(token, imageUrl);
+    }
+
+    @Transactional
+    public Member updateNickName(String token, String nickName) {
+        return memberRepository.updateNickName(token, nickName);
+    }
+
+    @Transactional
+    public Member updateExpAndLevel(String token, int addExp) {
+        return memberRepository.updateExpAndLevel(token, addExp);
+    }
+
+    public boolean isNickRedundant(String nickName) {
+        return memberRepository.findByNickName(nickName).isPresent();
+    }
+
+    public boolean isTokenExist(String token) {
+        return memberRepository.findByToken(token).isPresent();
+    }
+
+    public Member findById(int id) {
+        return memberRepository.findById(id).orElseThrow(() -> new RuntimeException("Member not found"));
+    }
+
+    public Member findByToken(String token) {
+        return memberRepository.findByToken(token).orElseThrow(() -> new RuntimeException("Member not found"));
+    }
+
+    public List<Member> getAll() {
+        return memberRepository.findAll();
+    }
 }
+
