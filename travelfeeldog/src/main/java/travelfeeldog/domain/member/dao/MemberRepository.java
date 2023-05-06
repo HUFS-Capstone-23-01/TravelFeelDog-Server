d02f85c78ef38d6fdc0ab43800dad442cd19867e
seungyeobchoi <98461038+seungyeobchoi@users.noreply.github.com>
2023-05-06 23:54:04 +0900

    refactor : refact code and reform to Google Java Convention

--- travelfeeldog/src/main/java/travelfeeldog/domain/member/dao/MemberRepository.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/member/dao/MemberRepository.java
@@ -1,7 +1,6 @@
 package travelfeeldog.domain.member.dao;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import travelfeeldog.domain.member.model.Member;
 
@@ -17,59 +16,90 @@ public class MemberRepository {
     @PersistenceContext
     private final EntityManager em;
 
-    public Optional<Member> saveMember(Long id, String nickName, int level, int exp, String imageUrl, String token) {
-        //DB에 회원 정보 저장
+    public Optional<Member> saveMember(String nickName, int level, int exp, String imageUrl, String token) {
         try {
-            findByToken(token).ifPresent( m -> {
+            findByToken(token).ifPresent(m -> {
                 throw new IllegalStateException("가입되어 있습니다.");
             });
-            Member member = Member.create(id, nickName, level, exp, imageUrl, token);
+            Member member = Member.create(nickName, level, exp, imageUrl, token);
             em.persist(member);
             return Optional.of(member);
         } catch (IllegalStateException e) {
             return Optional.empty();
         }
     }
-    public Optional<Member> findByToken(String token) {//토큰 검색
+
+    public Optional<Member> findByToken(String token) {
         try {
-            Member member = em.createQuery("select m from Member m where m.token = :token", Member.class).setParameter("token", token).getSingleResult();
+            Member member = em.createQuery("SELECT m FROM Member m WHERE m.token = :token", Member.class)
+                    .setParameter("token", token)
+                    .getSingleResult();
             return Optional.of(member);
         } catch (NoResultException e) {
             return Optional.empty();
         }
     }
-    public Optional<Member> findByNickName(String nickName) { //닉네임 검색
+
+    public Optional<Member> findByNickName(String nickName) {
         try {
-            Member member = em.createQuery("select m from Member m where m.nickName = :nickName", Member.class).setParameter("nickName", nickName).getSingleResult();
+            Member member = em.createQuery("SELECT m FROM Member m WHERE m.nickName = :nickName", Member.class)
+                    .setParameter("nickName", nickName)
+                    .getSingleResult();
             return Optional.of(member);
         } catch (NoResultException e) {
             return Optional.empty();
         }
     }
-    public void deleteMember(String inputToken) {//회원탈퇴
+
+    public Optional<Member> findById(int id) {
+        try {
+            Member member = em.createQuery("SELECT m FROM Member m WHERE m.id = :id", Member.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            return Optional.of(member);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    public void deleteMember(String inputToken) {
         Member member = findByToken(inputToken).get();
         em.remove(member);
     }
+
     public Member updateMemberImageUrl(String memberToken, String newUrl) {
-        //회원사진 업데이트
         Member member = findByToken(memberToken).get();
         member.setImageUrl(newUrl);
         em.merge(member);
         return member;
     }
+
     public Member updateNickName(String memberToken, String newNick) {
-        //회원닉네임 수정(중복 검사 포함)
         Member member = findByToken(memberToken).get();
-        findByNickName(newNick).ifPresent( m -> {
-            throw new IllegalStateException("이미 존재하는 닉네임입니다.");}
-        );
+        findByNickName(newNick).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 닉네임입니다.");
+        });
         member.setNickName(newNick);
         em.merge(member);
         return member;
     }
-    public List<Member> findAll() { //전체 조회
-        return em.createQuery("select m from Member m", Member.class).getResultList();
+
+    public Member updateExpAndLevel(String memberToken, int addingExp) {
+        Member member = findByToken(memberToken).get();
+        int changedExp = member.getExp() + addingExp;
+        if (changedExp / 40 == 0) {
+            member.setExp(changedExp);
+        } else {
+            member.setExp(changedExp % 40);
+            int newLevel = member.getLevel() + 1;
+            member.setLevel(newLevel);
+        }
+        em.merge(member);
+        return member;
     }
 
-
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }
 }
