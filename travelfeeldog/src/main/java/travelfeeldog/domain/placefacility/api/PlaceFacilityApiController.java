c0bdbc48fb143df0afd3bcd147c20e06be8266ea
chosunghyun18 <krismon@naver.com>
2023-05-07 23:43:34 +0900

    chore : change lines

--- travelfeeldog/src/main/java/travelfeeldog/domain/placefacility/api/PlaceFacilityApiController.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/placefacility/api/PlaceFacilityApiController.java
@@ -1,15 +1,14 @@
 package travelfeeldog.domain.placefacility.api;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import travelfeeldog.domain.facility.model.Facility;
 import travelfeeldog.domain.facility.service.FacilityService;
 import travelfeeldog.domain.place.dto.PlaceDtos.PlaceDetailDto;
@@ -22,36 +21,41 @@ import travelfeeldog.global.common.dto.ApiResponse;
 @RequestMapping("/places/{placeId}/facilities")
 @RequiredArgsConstructor
 public class PlaceFacilityApiController {
-
     private final PlaceFacilityService placeFacilityService;
     private final PlaceService placeService;
     private final FacilityService facilityService;
 
     @PostMapping
-    public ApiResponse<PlaceDetailDto> addFacilityToPlace(@PathVariable Long placeId, @RequestParam Long facilityId) {
+    public ApiResponse<PlaceDetailDto> addFacilityToPlace(@PathVariable Long placeId,
+                                                          @RequestParam Long facilityId) {
         Place place = placeService.getPlaceById(placeId);
         Facility facility = facilityService.getFacilityById(facilityId);
         placeFacilityService.addFacilityToPlace(place, facility);
         return ApiResponse.success(new PlaceDetailDto(place));
     }
 
-    @PostMapping(value = "/batch",produces = "application/json;charset=UTF-8")
-    public ApiResponse<Void> addFacilitiesToPlace(@PathVariable Long placeId, @RequestParam List<Long> facilityIds) {
+    @PostMapping(value = "/batch", produces = "application/json;charset=UTF-8")
+    public ApiResponse<Void> addFacilitiesToPlace(@PathVariable Long placeId,
+                                                  @RequestParam List<Long> facilityIds) {
         Place place = placeService.getPlaceById(placeId);
         List<Facility> facilities = facilityService.getFacilitiesByIds(facilityIds);
         placeFacilityService.addFacilitiesToPlace(place, facilities);
         return ApiResponse.success(HttpStatus.CREATED);
     }
-    @PostMapping(value = "/batch/name",produces = "application/json;charset=UTF-8")
-    public ApiResponse<PlaceDetailDto> addFacilitiesByNameToPlace(@PathVariable Long placeId, @RequestParam("names") List<String> names) {
+
+    @PostMapping(value = "/batch/name", produces = "application/json;charset=UTF-8")
+    public ApiResponse<PlaceDetailDto> addFacilitiesByNameToPlace(@PathVariable Long placeId,
+                                                                  @RequestParam("names") List<String> names) {
         Place place = placeService.getPlaceById(placeId);
         List<Facility> facilities = facilityService.getFacilitiesByNames(names);
+        facilities = facilities.stream().sorted(Comparator.comparing(Facility::getName)).collect(Collectors.toList());
         placeFacilityService.addFacilitiesToPlace(place, facilities);
         return ApiResponse.success(new PlaceDetailDto(place));
     }
 
     @DeleteMapping(value = "/{facilityId}", produces = "application/json;charset=UTF-8")
-    public ApiResponse<Void> removeFacilityFromPlace(@PathVariable Long placeId, @PathVariable Long facilityId) {
+    public ApiResponse<Void> removeFacilityFromPlace(@PathVariable Long placeId,
+                                                     @PathVariable Long facilityId) {
         Place place = placeService.getPlaceById(placeId);
         Facility facility = facilityService.getFacilityById(facilityId);
         placeFacilityService.removeFacilityFromPlace(place, facility);
@@ -64,5 +68,4 @@ public class PlaceFacilityApiController {
         placeFacilityService.removeAllFacilitiesFromPlace(place);
         return ApiResponse.success(HttpStatus.OK);
     }
-
 }
