e0ff64646a6bba57902a97a07444bb8465393385
chosunghyun18 <krismon@naver.com>
2023-05-09 21:57:03 +0900

    feat : add dto class and search method

--- travelfeeldog/src/main/java/travelfeeldog/domain/place/dao/PlaceRepository.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/place/dao/PlaceRepository.java
@@ -1,10 +1,32 @@
 package travelfeeldog.domain.place.dao;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import travelfeeldog.domain.place.dto.PlaceSearchResponseDto;
 import travelfeeldog.domain.place.model.Place;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place,Long> {
     Place findByName(String name);
+    @Query("SELECT DISTINCT new travelfeeldog.domain.place.dto.PlaceSearchResponseDto(p.id,p.thumbNailImageUrl,p.name,p.address,ps.reviewCountGood)" +
+            "FROM Place p " +
+            "JOIN p.placeStatic ps " +
+            "JOIN p.location l " +
+            "JOIN p.category c " +
+            "JOIN p.reviews r " +
+            "JOIN r.reviewGoodKeyWords gk " +
+            "JOIN gk.goodKeyWord gkw " +
+            "WHERE " +
+            "l.name = :locationName " +
+            "AND c.name = :categoryName " +
+            "AND gkw.keyWordName = :goodKeywordName ")
+    List<PlaceSearchResponseDto> findByNameAndLocationAndCategoryAndKeyWord(
+            @Param("locationName") String locationName,
+            @Param("categoryName") String categoryName,
+            @Param("goodKeywordName") String goodKeywordName);
+
 }
--- travelfeeldog/src/main/java/travelfeeldog/domain/review/model/RecommendStatus.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/review/model/RecommendStatus.java
@@ -2,5 +2,17 @@ package travelfeeldog.domain.review.model;
 
 
 public enum RecommendStatus {
-    GOOD,IDK,BAD;
+    GOOD("GOOD"),
+    IDK("IDK"),
+    BAD("BAD");
+
+    private String value;
+
+    RecommendStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
