a47562ca8de2994afb6a8ae12e53cb90ec153a00
chosunghyun18 <krismon@naver.com>
2023-05-15 13:30:54 +0900

    feat : add review keyword repository query

--- travelfeeldog/src/main/java/travelfeeldog/domain/review/reviewkeyword/dao/ReviewGoodKeyWordRepository.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/review/reviewkeyword/dao/ReviewGoodKeyWordRepository.java
@@ -2,9 +2,12 @@ package travelfeeldog.domain.review.reviewkeyword.dao;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import travelfeeldog.domain.review.reviewkeyword.model.ReviewGoodKeyWord;
 @Repository
 public interface ReviewGoodKeyWordRepository extends JpaRepository<ReviewGoodKeyWord,Long> {
-    List<Long> getAllGoodKeyWordIds(Long reviewId);
+    @Query("SELECT rgkw.goodKeyWord.id FROM ReviewGoodKeyWord rgkw WHERE rgkw.review.id = :reviewId")
+    List<Long> getAllGoodKeyWordIds(@Param("reviewId") Long reviewId);
 }
