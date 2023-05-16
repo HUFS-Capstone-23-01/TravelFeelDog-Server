da600b20f2745c2bf963ab2fd627f48487987673
chosunghyun18 <krismon@naver.com>
2023-05-16 18:23:42 +0900

    fix : fix repository

--- travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/dao/ScrapRepository.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/dao/ScrapRepository.java
@@ -4,13 +4,15 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import travelfeeldog.domain.feed.feed.model.Feed;
 import travelfeeldog.domain.feed.scrap.model.Scrap;
 
 public interface ScrapRepository extends JpaRepository<Scrap,Long> {
-//    List<Feed> findAllFeedByMemberId(Long memberId);
-    @Query("SELECT s " +
+    List<Feed> findAllFeedByMemberId(Long memberId);
+    @Query("SELECT DISTINCT s " +
             "FROM Scrap s " +
-            "WHERE " + "s.member.id = :memberId " + "AND s.feed.id = :feedId ")
-    Optional<Scrap> findScrapByMemberIdAndFeedId(Long memberId , Long feedId);
+            "WHERE s.member.id = :memberId AND s.feed.id = :feedId")
+    List<Scrap> findScrapsByMemberIdAndFeedId(Long memberId, Long feedId);
+
 }
