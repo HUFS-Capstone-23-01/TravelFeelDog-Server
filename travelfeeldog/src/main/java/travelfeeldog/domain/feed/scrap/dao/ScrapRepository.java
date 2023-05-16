674b3b67cb2a39928ddc4db03c970d66324f8b3b
chosunghyun18 <krismon@naver.com>
2023-05-16 18:01:11 +0900

    fix : scrap service

--- travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/dao/ScrapRepository.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/dao/ScrapRepository.java
@@ -1,10 +1,16 @@
 package travelfeeldog.domain.feed.scrap.dao;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import travelfeeldog.domain.feed.feed.model.Feed;
 import travelfeeldog.domain.feed.scrap.model.Scrap;
 
 public interface ScrapRepository extends JpaRepository<Scrap,Long> {
-    List<Feed> findAllFeedByMemberId(Long memberId);
+//    List<Feed> findAllFeedByMemberId(Long memberId);
+    @Query("SELECT s " +
+            "FROM Scrap s " +
+            "WHERE " + "s.member.id = :memberId " + "AND s.feed.id = :feedId ")
+    Optional<Scrap> findScrapByMemberIdAndFeedId(Long memberId , Long feedId);
 }
--- travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/model/Scrap.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/feed/scrap/model/Scrap.java
@@ -1,10 +1,8 @@
 package travelfeeldog.domain.feed.scrap.model;
 
-import java.time.LocalDateTime;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,16 +11,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import travelfeeldog.domain.feed.feed.model.Feed;
 import travelfeeldog.domain.member.model.Member;
 @Setter
 @Getter
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Scrap implements Persistable<String> {
+public class Scrap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "scrap_id", length = 100)
@@ -38,15 +32,9 @@ public class Scrap implements Persistable<String> {
     protected Scrap(){
 
     }
-    @CreatedDate
-    private LocalDateTime createdDate;
     public Scrap(Member member , Feed feed){
         this.member = member;
         this.feed =feed;
     }
 
-    @Override
-    public boolean isNew() {
-        return createdDate == null ;
-    }
 }
