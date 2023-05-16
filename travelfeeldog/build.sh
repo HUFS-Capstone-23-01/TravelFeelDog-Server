2baefa7d2df49d53c7777faab4f0e7158b63139f
chosunghyun18 <krismon@naver.com>
2023-05-16 20:23:14 +0900

    chore : add logging file pwd

--- travelfeeldog/build.sh
+++ travelfeeldog/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+LOG_FILE="/home/ubuntu/work/application.log"
+
 echo "start Build!"
 ./gradlew build
 PID="TravelFeelDog"
 echo $PID kill
 kill -9 $(ps -ef | grep $PID | grep -v grep | awk '{print $2 }')
 echo "start Application!"
-nohup java -jar ./build/libs/TravelFeelDog.jar &
+nohup java -jar ./build/libs/TravelFeelDog.jar >> "$LOG_FILE" 2>&1 &
+
 echo "Running Application"
