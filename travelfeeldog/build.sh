b711cd8ebc67ff9aadefa585940304439b862020
chosunghyun18 <krismon@naver.com>
2023-05-17 11:19:44 +0900

    feat : add remove the application.log - remove reent file before build spring application

--- travelfeeldog/build.sh
+++ travelfeeldog/build.sh
@@ -5,7 +5,9 @@ echo "start Build!"
 ./gradlew build
 PID="TravelFeelDog"
 echo $PID kill
+# shellcheck disable=SC2046
 kill -9 $(ps -ef | grep $PID | grep -v grep | awk '{print $2 }')
+rm -f "$LOG_FILE"
 echo "start Application!"
 nohup java -jar ./build/libs/TravelFeelDog.jar >> "$LOG_FILE" 2>&1 &
 
