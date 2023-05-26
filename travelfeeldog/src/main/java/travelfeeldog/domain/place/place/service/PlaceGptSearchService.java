2e91ceea5eb8a1a7d3f35a63d2c6d8f4a9b8d9c1
chosunghyun18 <krismon@naver.com>
2023-05-26 18:20:07 +0900

    refactor : change get gpt  answer by response body object - replace sysout into loginfo

--- travelfeeldog/src/main/java/travelfeeldog/domain/place/place/service/PlaceGptSearchService.java
+++ travelfeeldog/src/main/java/travelfeeldog/domain/place/place/service/PlaceGptSearchService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,9 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import travelfeeldog.domain.place.place.model.Place;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PlaceGptSearchService {
@@ -26,25 +26,23 @@ public class PlaceGptSearchService {
     private static final String END_POINT = "https://api.openai.com/v1/completions";
 
     public String answerText(String prompt, float temperature, int maxTokens) {
-
         List<Place> places = placeService.getAllPlaces();
-        List<Map<String, String>> placesNameAndDes = places.stream()
-                .map(p -> Map.of(p.getName(), p.getDescribe())) // Map으로 변환하여 이름과 설명을 저장합니다
+        List<String> selectedPlaces = places.stream()
+                .map(p -> p.getName() + ": " + p.getDescribe())
                 .collect(Collectors.toList());
-        Collections.shuffle(placesNameAndDes);
-        List<Map<String, String>> maxFiveElements = placesNameAndDes.subList(0, Math.min(3, placesNameAndDes.size()));
-        String selectedPlaces = maxFiveElements.stream()
-                .map(map -> String.join(": ", map.keySet().iterator().next(), map.values().iterator().next())) // 이름과 설명을 ":"로 구분하여 연결합니다
-                .collect(Collectors.joining(", ")); // 각 결과를 ", "로 구분하여 연결합니다
-        String query = String.format("%s 들 기반으로 질문에 답해줘 Q :%s", selectedPlaces, prompt);
-        System.out.println(query);
+
+        Collections.shuffle(selectedPlaces);
+        selectedPlaces = selectedPlaces.subList(0, Math.min(3, selectedPlaces.size()));
+
+        String query = String.format("%s 들 기반으로 질문에 답해줘 Q: %s", String.join(", ", selectedPlaces), prompt);
+        log.info("Query: {}", query);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer " + API_KEY);
 
         Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("model","text-davinci-003");
+        requestBody.put("model", "text-davinci-003");
         requestBody.put("prompt", query);
         requestBody.put("temperature", temperature);
         requestBody.put("max_tokens", maxTokens);
@@ -53,28 +51,33 @@ public class PlaceGptSearchService {
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<Map> response = restTemplate.postForEntity(END_POINT, requestEntity, Map.class);
-        String responseString = response.toString();
+        Map<String, Object> responseBody = response.getBody();
 
-        System.out.println(responseString);
+        List<Map<String, String>> choices = (List<Map<String, String>>) responseBody.get("choices");
+        if (choices != null && !choices.isEmpty()) {
+            String text = choices.get(0).get("text");
+            String result = extractResultFromText(text);
+            if (result != null) {
+                log.info("Result: {}", result);
+                return result;
+            }
+        }
 
-        String parsedResult = "";
-        int start = responseString.indexOf("choices=[{text=");
-        int end = responseString.indexOf("index=", start);
-
-        if (start != -1 && end != -1) {
-            parsedResult = responseString.substring(start, end).trim();
-        }
-        int cut = parsedResult.indexOf("A :");
-
-        if (cut != -1 ) {
-            parsedResult = parsedResult.substring(cut + 3).trim();
-        }
-        String result = parsedResult.replaceFirst("text=\\s*\\n\\s*\\d+\\.", "").replace("\n", "").replace("choices=[{text= !","");
-        if(result.isEmpty()){
-            return selectedPlaces;
-        }
-        else {
-            return result;
-        }
+        return String.join(", ", selectedPlaces);
     }
+
+    private String extractResultFromText(String text) {
+        int lastIndex = text.lastIndexOf("A:");
+        if (lastIndex != -1) {
+            return text.substring(lastIndex + 2).replaceAll("A :", "").replaceAll("\\n", "").trim();
+        }
+
+        int lastIndex1 = text.lastIndexOf("A :");
+        if (lastIndex1 != -1) {
+            return text.substring(lastIndex1 + 3).replaceAll("A :", "").replaceAll("\\n", "").trim();
+        }
+
+        return null;
+    }
+
 }
