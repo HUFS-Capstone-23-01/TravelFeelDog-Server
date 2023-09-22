e9059e5d1e27ab980e68d394b72ecfc0b7edd943
chosunghyun18 <krismon@naver.com>
2023-09-22 20:14:21 +0900

    fix  : ui only display role and user info

--- travelfeeldog/src/main/java/travelfeeldog/global/secure/auth/dto/SessionUser.java
+++ travelfeeldog/src/main/java/travelfeeldog/global/secure/auth/dto/SessionUser.java
@@ -9,10 +9,11 @@ public class SessionUser implements Serializable {
     private String nickName;
     private String email;
     private String imageUrl;
-
+    private String role;
     public SessionUser(Member user) {
         this.nickName = user.getNickName();
         this.email = user.getEmail();
         this.imageUrl = user.getImageUrl();
+        this.role = user.getRoleKey();
     }
 }
--- travelfeeldog/src/main/java/travelfeeldog/web/presentaion/IndexController.java
+++ travelfeeldog/src/main/java/travelfeeldog/web/presentaion/IndexController.java
@@ -4,26 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import travelfeeldog.domain.community.feed.service.FeedReadService;
 import travelfeeldog.global.secure.auth.LoginUser;
 import travelfeeldog.global.secure.auth.dto.SessionUser;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-
-    private final FeedReadService feedReadService;
-
-    @GetMapping("/home")
+    @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("posts", feedReadService.getListAll(1));
         if (user != null) {
             model.addAttribute("userName", user.getNickName());
+            model.addAttribute("userRole", user.getRole());  // role 정보 추가
         }
         return "index";
-    }
-    @GetMapping("/login/oauth2/code/google")
-    public String sho(){
-        return "hello world";
     }
 }
