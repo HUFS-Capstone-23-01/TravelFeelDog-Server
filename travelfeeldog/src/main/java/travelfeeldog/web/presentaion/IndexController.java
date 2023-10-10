9ae684c3db609e0db9375d4745fbc74c771f1621
chosunghyun18 <krismon@naver.com>
2023-10-10 14:23:36 +0900

    fix : modify seesion user - remove session handle

--- travelfeeldog/src/main/java/travelfeeldog/web/presentaion/IndexController.java
+++ travelfeeldog/src/main/java/travelfeeldog/web/presentaion/IndexController.java
@@ -5,16 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import travelfeeldog.global.secure.auth.LoginUser;
-import travelfeeldog.global.secure.auth.dto.SessionUser;
+import travelfeeldog.member.domain.model.Member;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
-        if (user != null) {
-            model.addAttribute("userName", user.getNickName());
-            model.addAttribute("userRole", user.getRole());  // role 정보 추가
+    public String index(Model model, @LoginUser Member member) {
+        if (member != null) {
+            model.addAttribute("userName", member.getNickName());
+            model.addAttribute("userRole", member.getRole());  // role 정보 추가
+        }else{
+            throw new RuntimeException();
         }
         return "index";
     }
