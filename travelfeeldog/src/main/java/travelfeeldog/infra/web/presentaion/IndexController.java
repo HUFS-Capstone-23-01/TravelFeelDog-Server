d0c17add2c5a7b22d48f2acdac65b3702d45f7a1
chosunghyun18 <krismon@naver.com>
2023-10-13 16:52:54 +0900

    fix : remove log

--- travelfeeldog/src/main/java/travelfeeldog/infra/web/presentaion/IndexController.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/web/presentaion/IndexController.java
@@ -2,7 +2,6 @@ package travelfeeldog.infra.web.presentaion;
 
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -12,15 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
-
 public class IndexController {
     @GetMapping("/")
     public String index(Model model,  @AuthenticationPrincipal OAuth2User oauth2User) {
         if (oauth2User != null) {
-            // OAuth2User에서 사용자 정보를 추출합니다.
             String userName = oauth2User.getAttribute("name");
             Collection<? extends GrantedAuthority> authorities = oauth2User.getAuthorities();
-
             model.addAttribute("userName", userName);
             model.addAttribute("userRole",authorities);
         }
