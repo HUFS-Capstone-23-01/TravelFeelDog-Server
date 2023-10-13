f6c1ba32c7403f20724b2bd21390069dec87eaf5
chosunghyun18 <krismon@naver.com>
2023-10-13 22:45:49 +0900

    fix : extract email loginuser annotiaion - remove bearer related code

--- travelfeeldog/src/main/java/travelfeeldog/global/auth/LoginUserArgumentResolver.java
+++ travelfeeldog/src/main/java/travelfeeldog/global/auth/LoginUserArgumentResolver.java
@@ -3,6 +3,7 @@ package travelfeeldog.global.auth;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -14,6 +15,7 @@ import travelfeeldog.global.auth.jwt.JwtService;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final JwtService jwtService;
@@ -26,7 +28,9 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
         String token = AuthorizationExtractor.extract(Objects.requireNonNull(request));
+        log.info("token {} ",token);
         if (token == null || token.trim().isEmpty()) {
             return null;
         }
--- travelfeeldog/src/main/java/travelfeeldog/infra/api/ConnectionCheckApi.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/api/ConnectionCheckApi.java
@@ -20,7 +20,7 @@ public class ConnectionCheckApi {
     @GetMapping(value="/{testNumber}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Long> getConnectTestNumber(@PathVariable Long testNumber , @LoginUser String email){
-        log.info("email{}",email);
+        log.info("email : {}",email);
         return ResponseEntity.ok(testNumber);
     }
 }
