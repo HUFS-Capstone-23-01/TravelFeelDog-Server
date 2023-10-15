30014caf64dfd94e0d8b094e60d29279d3d54934
chosunghyun18 <krismon@naver.com>
2023-10-15 16:44:17 +0900

    refact : remove log

--- travelfeeldog/src/main/java/travelfeeldog/global/auth/secure/LoginUserArgumentResolver.java
+++ travelfeeldog/src/main/java/travelfeeldog/global/auth/secure/LoginUserArgumentResolver.java
@@ -3,7 +3,7 @@ package travelfeeldog.global.auth.secure;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -12,10 +12,8 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import travelfeeldog.global.auth.jwt.service.JwtService;
 
-
 @RequiredArgsConstructor
 @Component
-@Slf4j
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final JwtService jwtService;
@@ -28,9 +26,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-
         String token = AuthorizationExtractor.extract(Objects.requireNonNull(request));
-        log.info("token {} ",token);
         if (token == null || token.trim().isEmpty()) {
             return null;
         }
