79ee3ffe93bf86ea734f357904c024132f9fdb5b
chosunghyun18 <krismon@naver.com>
2023-10-15 16:43:57 +0900

    feat : add exceptionHandling in SecurityConfig

--- travelfeeldog/src/main/java/travelfeeldog/global/auth/secure/SecurityConfig.java
+++ travelfeeldog/src/main/java/travelfeeldog/global/auth/secure/SecurityConfig.java
@@ -1,6 +1,5 @@
 package travelfeeldog.global.auth.secure;
 
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +7,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-import travelfeeldog.global.auth.jwt.config.CustomAuthenticationEntryPoint;
 import travelfeeldog.global.auth.jwt.filter.JwtFilter;
 import travelfeeldog.global.auth.jwt.service.JwtService;
 import travelfeeldog.infra.oauth2.service.CustomOAuth2UserService;
-import travelfeeldog.member.domain.model.Role;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,40 +24,17 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final JwtService jwtService;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(
-                        AbstractHttpConfigurer::disable
-                ).cors(
-                        AbstractHttpConfigurer::disable
-                )
-                .logout(withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(withDefaults());
         httpSecurity.oauth2Login(request -> request.userInfoEndpoint(
                         userInfoEndpointConfig -> userInfoEndpointConfig.userService(
                                 customOAuth2UserService)));
         httpSecurity.addFilterAfter(new JwtFilter(jwtService), LogoutFilter.class);
-//        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(
-//                                "/",
-//                                "/login",
-//                                "/app/index.js",
-//                                "/favicon.ico",
-//                                "/app/index.js",
-//                                "**/image/**",
-//                                "/oauth2/authorization/google",
-//                                "/login/oauth2/code/google",
-//                                "/actuator/health",
-//                                "**/swagger-ui/**","/usage" // for swagger
-//                        ).permitAll()
-//                        .requestMatchers("**/admin/**").hasRole(Role.ADMIN.name())
-//                        .requestMatchers("**/user/**").hasRole(Role.USER.name())
-//                        .anyRequest()
-//                        .authenticated()
-//        );
-
-        // 추가하면 테스트 페이지 에러 + hasRole 개판
-        httpSecurity.exceptionHandling((exception)-> exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+        httpSecurity.exceptionHandling((exception)-> exception.authenticationEntryPoint(authenticationEntryPoint));
 
         return httpSecurity.build();
     }
