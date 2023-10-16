4df28565a19e9a706507b8f603f9946309a499ae
chosunghyun18 <krismon@naver.com>
2023-10-16 14:47:23 +0900

    test : remove log in jwt provider test

--- travelfeeldog/src/test/java/travelfeeldog/global/auth/jwt/service/JwtProviderTest.java
+++ travelfeeldog/src/test/java/travelfeeldog/global/auth/jwt/service/JwtProviderTest.java
@@ -8,42 +8,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.jsonwebtoken.Claims;
 import java.util.Date;
-import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import travelfeeldog.global.auth.jwt.config.JwtSecretKey;
 import travelfeeldog.global.auth.jwt.exception.InvalidTokenException;
 
-@Slf4j
 class JwtProviderTest {
+    String jwtSecretKey = "ThisIsASeTESTESTSETSTdkdkkskskkskskskkskskkskskaqqqqqq";
     @Test
     void validateExpiredToken_expired_check_test() {
-        String jwtSecretKey = "ThisIsASecretKeyAndShouldBeAtLeast32Charsckdkdkkskskkskskskkskskkskskaqqqqqq";
+
         JwtProvider jwtProvider = new JwtProvider(new JwtSecretKey(jwtSecretKey, 0L, 0L));
         String payload = "11c7b743ff@gmail.com";
-
         String atk = jwtProvider.createAccessToken(payload).get("accessToken");
-        log.info("atk : {}",atk);
-
         assertNotNull(atk);
         assertFalse(atk.isEmpty());
-
         assertTrue(jwtProvider.isTokenExpire(atk));
-        // 토큰 유효성 검증에서 예외가 발생해야 합니다.
         assertThrows(InvalidTokenException.class, () -> jwtProvider.validateToken(atk));
     }
     @Test
     void check_time_ExpiredToken() {
-        String jwtSecretKey = "ThisIsASecretKeyAndShouldBeAtLeast32Charsckdkdkkskskkskskskkskskkskskaqqqqqq";
+        long millSecondAccessTime  = 30000000L; // 8 시간 19분 59초
 
-        // 만료 시간을 0으로 설정하여 즉시 만료되는 토큰을 생성합니다.
-        JwtProvider jwtProvider = new JwtProvider(new JwtSecretKey(jwtSecretKey, 1000L, 11111L));
+        JwtProvider jwtProvider = new JwtProvider(new JwtSecretKey(jwtSecretKey, millSecondAccessTime, 11111L));
         String payload = "11c7b743ff@gmail.com";
 
         String atk = jwtProvider.createAccessToken(payload).get("accessToken");
-        System.out.println(atk);
+//        System.out.println(atk);
 
         Claims claims = jwtProvider.extractClaims(atk);
-        log.info("claims : {}",claims);
+
         // 만료 시간과 현재 시간을 가져와 남은 시간 계산
         Date expirationDate = claims.getExpiration();
         long millisUntilExpiration = expirationDate.getTime() - System.currentTimeMillis();
@@ -51,10 +46,12 @@ class JwtProviderTest {
         long minutesUntilExpiration = secondsUntilExpiration / 60;
         long hoursUntilExpiration = minutesUntilExpiration / 60;
 
-        // 로그에 남은 시간 출력
-        log.info("claims : {}", claims);
-        log.info("토큰이 만료되기까지 남은 시간: {}시간 {}분 {}초",
-                hoursUntilExpiration, minutesUntilExpiration % 60, secondsUntilExpiration % 60);
+//        log.info("claims : {}", claims);
+//        log.info("토큰이 만료되기까지 남은 시간: {}시간 {}분 {}초", hoursUntilExpiration, minutesUntilExpiration % 60, secondsUntilExpiration % 60);
+
+        Assertions.assertEquals(secondsUntilExpiration%60,59);
+        Assertions.assertEquals(minutesUntilExpiration % 60,19);
+        Assertions.assertEquals(hoursUntilExpiration,8);
         assertNotNull(atk);
 
     }
