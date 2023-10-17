7a15a3a98ab2bbf4e468812845efc2be6e1d08c9
chosunghyun18 <krismon@naver.com>
2023-10-17 18:29:54 +0900

    refact : remove modify path variable into request param

--- travelfeeldog/src/main/java/travelfeeldog/infra/api/ConnectionCheckApi.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/api/ConnectionCheckApi.java
@@ -1,33 +1,29 @@
 package travelfeeldog.infra.api;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import travelfeeldog.global.auth.secure.LoginUser;
+
 
 @RestController
-@RequestMapping("/api/v1/test")
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 //@Slf4j
 public class ConnectionCheckApi {
 
-//    @GetMapping(value="/{testNumber}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public ResponseEntity<Long> getConnectAdminTestNumber(@PathVariable Long testNumber) {
-//  //      log.info("email : {}",email);
-//        return ResponseEntity.ok(testNumber);
-//    }
-
-    @GetMapping(value="/{testNumber}")
+    @GetMapping(value="/test")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Long> getConnectUserTestNumber(@PathVariable Long testNumber){
+    public ResponseEntity<Long> getConnectUserTestNumber(@RequestParam Long testNumber,
+                                                        @RequestHeader("Authorization") String token){
 //        log.info("email : {}",email);
         return ResponseEntity.ok(testNumber);
     }
