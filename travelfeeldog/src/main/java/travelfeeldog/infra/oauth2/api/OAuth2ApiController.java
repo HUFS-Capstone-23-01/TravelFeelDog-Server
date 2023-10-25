e9ba2855c92ccfaf0e621ac79722851d4d9fb20d
chosunghyun18 <krismon@naver.com>
2023-10-25 20:48:40 +0900

    feat : add io token response web

--- travelfeeldog/src/main/java/travelfeeldog/infra/oauth2/api/OAuth2ApiController.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/oauth2/api/OAuth2ApiController.java
@@ -1,10 +1,15 @@
 package travelfeeldog.infra.oauth2.api;
 
+import java.io.IOException;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import travelfeeldog.global.auth.jwt.service.JwtService;
 import travelfeeldog.global.common.dto.ApiResponse;
@@ -19,6 +24,7 @@ public class OAuth2ApiController {
     private final GoogleLoginService googleLoginService;
     private final JwtService jwtService;
 
+    // mobile 과 web 에서 스프링에서 발급한 jwt 을 통한 회원 정보 호출
     @PostMapping("/oauth2/token")
     public ApiResponse<TokenLoginResponse> googleAuthenticationLogin(
             @RequestBody TokenLoginRequest request) {
@@ -26,11 +32,39 @@ public class OAuth2ApiController {
         return ApiResponse.success(jwtService.getTokenLoginResponseByMember(member));
     }
 
+    // 웹 에서 구글 ID 토큰을 통한 회원 정보 호출  :  회원 이메일 타입, jwt
+    @PostMapping("/web/oauth2/google/idToken")
+    public ApiResponse<TokenLoginResponse> webGoogleAuthenticationLogin(
+            @RequestBody TokenLoginRequest request) {
+        Member member = googleLoginService.loginGoogleOAuthWithWebIdToken(request.token());
+        return ApiResponse.success(jwtService.getTokenLoginResponseByMember(member));
+    }
+
+    // mobile 에서 구글 ID 토큰을 통한 회원 정보 호출  :  회원 이메일 타입, jwt
     @PostMapping("/mobile/oauth2/google")
     public ApiResponse<TokenLoginResponse> mobileGoogleAuthenticationLogin(
             @RequestBody TokenLoginRequest request) {
-        Member member = googleLoginService.loginGoogleOAuthWithIdToken(request.token());
+        Member member = googleLoginService.loginGoogleOAuthWithMobileIdToken(request.token());
         return ApiResponse.success(jwtService.getTokenLoginResponseByMember(member));
+    }
+
+    // Spring , React 에서 IO 토큰 발급을 위한 Enrty Point , 접속시 구글 로그인 창으로 이동
+    // http://localhost:8080/login/web/oauth2/google
+    @GetMapping("/web/oauth2/google")
+    public ResponseEntity<Void> redirectToGoogleLogin() {
+        String googleLoginUrl = googleLoginService.redirectToGoogleLogin();
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(googleLoginUrl)).build();
+    }
+
+    // 구글 로그인 이후 ID 토큰을 발급받는  redirection url
+    @GetMapping("/web/oauth2/code/google")
+    public ResponseEntity<String> handleGoogleLoginCallback(
+            @RequestParam String code,
+            @RequestParam String state) throws IOException {
+        String token = googleLoginService.getIdTokenFromGoogle(code);
+
+//        String token = googleLoginService.getAccessTokenFromGoogle(code);
+        return ResponseEntity.ok(token);
     }
 
 }
--- travelfeeldog/src/main/java/travelfeeldog/infra/oauth2/service/GoogleLoginService.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/oauth2/service/GoogleLoginService.java
@@ -1,15 +1,19 @@
 package travelfeeldog.infra.oauth2.service;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import travelfeeldog.infra.oauth2.dto.OAuthAttributes;
@@ -17,18 +21,50 @@ import travelfeeldog.member.domain.application.service.MemberWriteService;
 import travelfeeldog.member.domain.model.Member;
 
 @Service
-@RequiredArgsConstructor
 public class GoogleLoginService {
 
-    @Value("${spring.security.oauth2.client.registration.google-mobile.client-id}")
-    String clientId;
-    private final GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
-            new NetHttpTransport(), new JacksonFactory())
-            .setAudience(Collections.singletonList(clientId))
-            .build();
+    private final String mobileClientId;
+    private GoogleIdTokenVerifier verifier;
     private final MemberWriteService memberWrite;
+    private final String webClientId;
+    private final String webClientSecret;
+    private final String redirectWebUrl;
+    private static final List<String> SCOPES = Arrays.asList(
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/userinfo.profile"
+    );
 
-    public Member loginGoogleOAuthWithIdToken(String idToken) {
+    @Autowired
+    public GoogleLoginService(
+            @Value("${spring.security.oauth2.client.registration.google-mobile.client-id}") String mobileClientId,
+            @Value("${spring.security.oauth2.client.registration.google-web-react.client-id}") String webClientId,
+            @Value("${spring.security.oauth2.client.registration.google-web-react.client-secret}") String webClientSecret,
+            @Value("${spring.security.oauth2.client.registration.google-web-react.redirect-uri}") String redirectWebUrl,
+            MemberWriteService memberWrite) {
+        this.mobileClientId = mobileClientId;
+        this.webClientId = webClientId;
+        this.webClientSecret = webClientSecret;
+        this.memberWrite = memberWrite;
+        this.redirectWebUrl = redirectWebUrl;
+    }
+
+    public Member loginGoogleOAuthWithMobileIdToken(String idToken) {
+        setVerifier(this.mobileClientId);
+        return saveMemberByAttributes(idToken);
+    }
+
+    public Member loginGoogleOAuthWithWebIdToken(String idToken) {
+        setVerifier(this.webClientId);
+        return saveMemberByAttributes(idToken);
+    }
+
+    private void setVerifier(String clientId) {
+        this.verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+                .setAudience(Collections.singletonList(clientId))
+                .build();
+    }
+
+    private Member saveMemberByAttributes(String idToken) {
         OAuthAttributes attributes = verifyGoogleIDToken(idToken);
         return memberWrite.saveByAttributes(attributes);
     }
@@ -37,8 +73,8 @@ public class GoogleLoginService {
         GoogleIdToken idTokenObj;
         try {
             idTokenObj = verifier.verify(idToken);
-        } catch (GeneralSecurityException | IOException | RuntimeException e) {
-            throw new IllegalArgumentException("Wrong In verify IdToken");
+        } catch (GeneralSecurityException | IOException e) {
+            throw new IllegalArgumentException("Wrong In verify IdToken", e);
         }
         Payload payload = idTokenObj.getPayload();
         OAuthAttributes attributes = OAuthAttributes.ofGoogle("sub", payload);
@@ -48,4 +84,35 @@ public class GoogleLoginService {
         return attributes;
     }
 
+    public String redirectToGoogleLogin() {
+        return buildGoogleAuthorizationCodeFlow()
+                .newAuthorizationUrl()
+                .setRedirectUri(this.redirectWebUrl)
+                .setState("state_parameter_passthrough_value")
+                .build();
+    }
+
+    public String getIdTokenFromGoogle(String code) throws IOException {
+        return getTokenResponseFromGoogle(code).getIdToken();
+    }
+
+    public String getAccessTokenFromGoogle(String code) throws IOException {
+        return getTokenResponseFromGoogle(code).getAccessToken();
+    }
+
+    private GoogleTokenResponse getTokenResponseFromGoogle(String code) throws IOException {
+        return buildGoogleAuthorizationCodeFlow()
+                .newTokenRequest(code)
+                .setRedirectUri(redirectWebUrl)
+                .execute();
+    }
+
+    private GoogleAuthorizationCodeFlow buildGoogleAuthorizationCodeFlow() {
+        return new GoogleAuthorizationCodeFlow.Builder(
+                new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
+                webClientId, webClientSecret, SCOPES)
+                .setAccessType("offline")
+                .setApprovalPrompt("force")
+                .build();
+    }
 }
