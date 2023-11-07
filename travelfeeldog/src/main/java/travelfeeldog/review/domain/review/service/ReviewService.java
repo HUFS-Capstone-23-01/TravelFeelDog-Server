3a78ab973eed5db4e75e5dfe779d5d3a61f61bfa
chosunghyun18 <krismon@naver.com>
2023-11-07 12:50:28 +0900

    refact : change collect into toList() for unmodifiable

--- travelfeeldog/src/main/java/travelfeeldog/review/domain/review/service/ReviewService.java
+++ travelfeeldog/src/main/java/travelfeeldog/review/domain/review/service/ReviewService.java
@@ -2,27 +2,23 @@ package travelfeeldog.review.domain.review.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import lombok.RequiredArgsConstructor;
-
-import travelfeeldog.member.domain.model.Member;
 import travelfeeldog.member.domain.application.service.MemberService;
+import travelfeeldog.member.domain.model.Member;
 import travelfeeldog.place.domain.place.model.Place;
 import travelfeeldog.place.domain.place.service.PlaceService;
+import travelfeeldog.review.domain.review.model.RecommendStatus;
+import travelfeeldog.review.domain.review.model.Review;
+import travelfeeldog.review.domain.review.model.ReviewImage;
 import travelfeeldog.review.domain.review.repositoy.ReviewImageRepository;
 import travelfeeldog.review.domain.review.repositoy.ReviewRepository;
+import travelfeeldog.review.domain.reviewkeyword.service.ReviewKeyWordService;
 import travelfeeldog.review.dto.ReviewDtos.ReviewMemberPageResponseDto;
 import travelfeeldog.review.dto.ReviewDtos.ReviewPageResponseDto;
 import travelfeeldog.review.dto.ReviewDtos.ReviewPostRequestDto;
 import travelfeeldog.review.dto.ReviewDtos.UpdateReviewImageDto;
-import travelfeeldog.review.domain.review.model.RecommendStatus;
-import travelfeeldog.review.domain.review.model.Review;
-import travelfeeldog.review.domain.review.model.ReviewImage;
-import travelfeeldog.review.domain.reviewkeyword.service.ReviewKeyWordService;
 
 @Transactional(readOnly = true)
 @Service
@@ -33,23 +29,6 @@ public class ReviewService {
     private final PlaceService placeService;
     private final MemberService memberService;
     private final ReviewKeyWordService reviewKeyWordService;
-
-    public List<ReviewMemberPageResponseDto> getAllReviews() {
-        return reviewRepository.findAll()
-                .stream()
-                .map(ReviewMemberPageResponseDto::new)
-                .collect(Collectors.toList());
-    }
-
-    public Review getReviewById(Long reviewId) {
-        return reviewRepository.findById(reviewId)
-                .orElseThrow(() -> new EntityNotFoundException("Review not found with id: " + reviewId));
-    }
-
-    public ReviewMemberPageResponseDto getReviewMemberPageByReviewId(Long reviewId) {
-        Review review = getReviewById(reviewId);
-        return new ReviewMemberPageResponseDto(review);
-    }
 
     @Transactional
     public ReviewPageResponseDto saveReview(String token, ReviewPostRequestDto request) {
@@ -66,19 +45,37 @@ public class ReviewService {
     }
 
     @Transactional
+    public String updateReviewImage(UpdateReviewImageDto reviewImageDto) {
+        ReviewImage reviewImage = reviewImageRepository.findByReviewId(reviewImageDto.reviewId())
+                .orElseThrow(
+                        () -> new EntityNotFoundException("Review not found with id: " + reviewImageDto.reviewId()));
+        reviewImage.updateImage(reviewImageDto.imageUrl());
+        return reviewImageDto.imageUrl();
+    }
+
+    @Transactional
     public void deleteReviewById(Long id) {
         reviewRepository.deleteById(id);
     }
 
-    public List<ReviewPageResponseDto> getReviewsByPlaceId(Long placeId) {
-        return reviewRepository.findByPlaceId(placeId)
+    public List<ReviewMemberPageResponseDto> getAllReviews() {
+        return reviewRepository.findAll()
                 .stream()
-                .map(ReviewPageResponseDto::new)
-                .collect(Collectors.toList());
+                .map(ReviewMemberPageResponseDto::new)
+                .toList();
     }
 
-    public List<ReviewPageResponseDto> getReviewsByQuery(String token, Long placeId, String request) {
-        Member member = memberService.findByToken(token);
+    public Review getReviewById(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new EntityNotFoundException("Review not found with id: " + reviewId));
+    }
+
+    public ReviewMemberPageResponseDto getReviewMemberPageByReviewId(Long reviewId) {
+        Review review = getReviewById(reviewId);
+        return new ReviewMemberPageResponseDto(review);
+    }
+
+    public List<ReviewPageResponseDto> getReviewsByQuery(Long placeId, String request) {
         List<Review> reviews = "TIME".equalsIgnoreCase(request)
                 ? reviewRepository.findAllByPlaceIdOrderByCreatedDateTimeDesc(placeId)
                 : reviewRepository.findByPlaceIdAndRecommendStatus(placeId,
@@ -86,7 +83,7 @@ public class ReviewService {
         return reviews
                 .stream()
                 .map(ReviewPageResponseDto::new)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public List<ReviewMemberPageResponseDto> getReviewsByMemberId(String token) {
@@ -94,17 +91,6 @@ public class ReviewService {
         return reviewRepository.findByMemberId(memberId)
                 .stream()
                 .map(ReviewMemberPageResponseDto::new)
-                .collect(Collectors.toList());
-    }
-
-    @Transactional
-    public String updateReviewImage(String token, UpdateReviewImageDto reviewImageDto) {
-        memberService.findByToken(token);
-        ReviewImage reviewImage = reviewImageRepository.findByReviewId(reviewImageDto.reviewId())
-                .orElseThrow(
-                        () -> new EntityNotFoundException("Review not found with id: " + reviewImageDto.reviewId()));
-        ;
-        reviewImage.updateImage(reviewImageDto.imageUrl());
-        return reviewImageDto.imageUrl();
+                .toList();
     }
 }
