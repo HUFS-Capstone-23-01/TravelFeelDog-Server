6c51d34fa144cfa23aac36a73e4036a0b872101f
chosunghyun18 <krismon@naver.com>
2023-12-01 14:21:52 +0900

    chore : add server item swagger

--- travelfeeldog/src/main/java/travelfeeldog/infra/swagger/SwaggerConfig.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/swagger/SwaggerConfig.java
@@ -1,12 +1,16 @@
 package travelfeeldog.infra.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().addServersItem(new Server().url("/"));
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://api.sunghyun98.com").description("Test server"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local server"))
+                .info(new Info().title("API").description("API Description").version("1.0.0"));
     }
 }
