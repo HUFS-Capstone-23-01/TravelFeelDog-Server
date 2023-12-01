baf38a6232be540ff427ed48d4ae4c5d36601406
chosunghyun18 <krismon@naver.com>
2023-12-01 14:52:36 +0900

    feat : add servers in swagger

--- travelfeeldog/src/main/java/travelfeeldog/infra/swagger/SwaggerConfig.java
+++ travelfeeldog/src/main/java/travelfeeldog/infra/swagger/SwaggerConfig.java
@@ -4,13 +4,15 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://api.sunghyun98.com").description("Test server"))
-                .addServersItem(new Server().url("http://localhost:8080").description("Local server"))
+                .addServersItem(new Server().url("https://api.sunghyun98.com/").description("Test server"))
+                .addServersItem(new Server().url("http://localhost:8080/").description(" Local server"))
                 .info(new Info().title("API").description("API Description").version("1.0.0"));
     }
 }
